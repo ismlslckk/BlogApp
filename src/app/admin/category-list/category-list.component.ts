c7cde7d55a07124463e43f977044df0ffe91ae22
ismlslckk <ismlslck@gmail.com>
2018-09-23 22:04:23 +0300

    afterViewInit kaldirildi.

--- src/app/admin/category-list/category-list.component.ts
+++ src/app/admin/category-list/category-list.component.ts
@@ -7,18 +7,8 @@ import {CategoryService} from './category.service'
   styleUrls: ['./category-list.component.css'],
   providers:[CategoryService]
 })
-export class CategoryListComponent implements AfterViewInit,OnInit  {
-  async ngAfterViewInit() {
-    await this.loadScript('/assets/admin/js/lib/datatables/datatables.min.js');
-    await this.loadScript('/assets/admin/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js');
-    await this.loadScript('/assets/admin/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js');
-    await this.loadScript('/assets/admin/js/lib/datatables/cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js');
-    await this.loadScript('/assets/admin/js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js');
-    await this.loadScript('/assets/admin/js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js');
-    await this.loadScript('/assets/admin/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js');
-    await this.loadScript('/assets/admin/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js');
-    await this.loadScript('/assets/admin/js/lib/datatables/datatables-init.js');
-  }
+export class CategoryListComponent implements OnInit  {
+   
   constructor(private categoryService:CategoryService) { }
 
   categories:Category[];
@@ -28,14 +18,5 @@ export class CategoryListComponent implements AfterViewInit,OnInit  {
   ngOnInit() {
     this.getCategories();
   }
-  
-  private loadScript(scriptUrl: string) {
-    return new Promise((resolve, reject) => {
-      const scriptElement = document.createElement('script');
-      scriptElement.src = scriptUrl;
-      scriptElement.onload = resolve;
-      document.body.appendChild(scriptElement);
-    })
-  }
-
+    
 }
